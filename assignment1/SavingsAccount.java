e784fba9ae673092120cb735965609eaa0108698
MelendezIvelis <MelendezIvelis@gmail.com>
2021-04-25 21:38:37 -0400

    Pair Programming Assignment

---
+++ assignment1/SavingsAccount.java
@@ -0,0 +1,54 @@
+package com.meritamerica.assignment1;
+
+public class SavingsAccount {
+
+	private double balance;
+	private double interestRate = 0.01;
+
+	// add constructor
+
+	public SavingsAccount(double balance) {
+		this.balance = balance;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public double getInterestRate() {
+		return interestRate;
+	}
+
+	public double futureValue(int years) {
+		double principle = balance;
+		double factor = 1.01;
+		return Math.pow(factor, years) * principle;
+	}
+
+	public boolean withdraw(double amount) {
+		if (amount < 0 || amount > balance) {
+			return false;
+		} else {
+			balance = balance - amount;
+			return true;
+		}
+
+	}
+
+	public boolean deposit(double amount) {
+		if (amount < 0) {
+			return false;
+		} else {
+			balance = balance + amount;
+			return true;
+	}
+}
+
+	@Override
+	public String toString() {
+		return "SavingsAccount [balance=" + balance + ", interestRate=" + interestRate + ", futureValue(3)="
+				+ futureValue(3) + "]";
+	}
+
+}
+
